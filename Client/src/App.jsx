a6154f10ded3e70cfad789a0eb57f35ddb17f5d8
Mohsin1016 <129897778+Mohsin1016@users.noreply.github.com>
2024-05-26 23:17:46 +0500

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = "https://chat-app-back-end-khaki.vercel.app/api";
+  axios.defaults.baseURL = "https://chat-app-back-end-khaki.vercel.app";
   axios.defaults.withCredentials = true;
   return (
     <>
