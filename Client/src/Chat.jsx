4e536b497531ef787e42fa934b91d5e3c4743e1c
asadkhalid566 <76807556+asadkhalid566@users.noreply.github.com>
2024-05-27 01:32:12 +0500

    gkkinin

--- Client/src/Chat.jsx
+++ Client/src/Chat.jsx
@@ -23,7 +23,7 @@ export default function Chat() {
   }, []);
 
   function connectToWs() {
-    const ws = new WebSocket("ws://localhost:4040");
+    const ws = new WebSocket("wss://chat-app-back-end-khaki.vercel.app");
     setWs(ws);
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", () => {
--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.options('*', cors(corsOptions)); // Enable pre-flight requests for all routes
+app.options('*', cors(corsOptions));
 
 
 app.get("/", (req, res) => {
