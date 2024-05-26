83fa6c6975e786efa37392aab150c910a9c1a05a
asadkhalid566 <76807556+asadkhalid566@users.noreply.github.com>
2024-05-27 01:17:33 +0500

    ggf

--- api/index.js
+++ api/index.js
@@ -32,14 +32,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-
 app.options('*', cors(corsOptions)); // Enable pre-flight requests for all routes
 
 
