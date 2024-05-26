0243c5a202f3411decfcb5dff2196ae3ee6d7f7a
asadkhalid566 <76807556+asadkhalid566@users.noreply.github.com>
2024-05-26 23:27:51 +0500

    ddd

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,6 +5,7 @@ import Routes from "./Routes";
 function App() {
   axios.defaults.baseURL = "https://chat-app-back-end-khaki.vercel.app/api";
   axios.defaults.withCredentials = true;
+  
   return (
     <>
       <UserContextProvider>
--- api/index.js
+++ api/index.js
@@ -26,6 +26,7 @@ app.use(
   })
 );
 
+
 app.get("/", (req, res) => {
   res.json("test is oky");
 });
